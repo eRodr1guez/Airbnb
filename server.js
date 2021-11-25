ee4f117a47c0a13ad10420aca6918f19640fa74a
Ernesto <erodrigueztoimil@gmail.com>
2021-11-24 19:21:12 -0500

    mongodb connection setup

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Connect DB
+connectDB();
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+const PORT = process.env.PORT || 8082;
+app.listen(PORT, () => console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT));
